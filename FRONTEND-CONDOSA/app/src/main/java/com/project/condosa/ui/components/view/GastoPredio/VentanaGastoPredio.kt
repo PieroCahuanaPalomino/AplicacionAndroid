e41bf213cac4eabae2dc74cb6ee3f3bb34610350
YeseniaTD <yeseniaandrea.torres@unmsm.edu.pe>
2023-10-25 16:37:28 -0500

    Creación de tabla Gastos Predio

--- FRONTEND-CONDOSA/app/src/main/java/com/project/condosa/ui/components/view/GastoPredio/VentanaGastoPredio.kt
+++ FRONTEND-CONDOSA/app/src/main/java/com/project/condosa/ui/components/view/GastoPredio/VentanaGastoPredio.kt
@@ -2,17 +2,34 @@ package com.project.condosa.ui.components.view.GastoPredio
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -29,7 +46,9 @@ fun GastosPredio(
     name: String, period: String
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF6F8FF))
     ) {
         // Encabezado de la aplicación
         Column(
@@ -74,6 +93,103 @@ fun GastosPredio(
                 fontWeight = FontWeight.Bold
             )
         }
+
+        // Tabla de gastos (LazyColumn)
+        Box (modifier=Modifier.padding(horizontal = 10.dp)
+            .clip(RoundedCornerShape(4))){
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(width = 400.dp, height = 340.dp)
+                    .background(Color(0xFFFFFFFF))
+                    .padding(vertical = 8.dp))
+            {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xFF000080))
+                            .clip(RoundedCornerShape(0.dp, 0.dp, 8.dp, 8.dp))
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Gasto",
+                                fontSize = 16.sp,
+                                fontFamily = poppins,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.weight(2.5f)
+                            )
+                            Text(
+                                text = "Monto (S/)",
+                                fontSize = 16.sp,
+                                fontFamily = poppins,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.weight(1.5f)
+                            )
+                            Text(
+                                text = "",
+                                fontSize = 16.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.weight(0.5f)
+                            )
+                        }
+                    }
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .width(400.dp) // Establece el ancho deseado para la tabla
+                            .background(Color(0xFFF6F8FF)) // Color de fondo de la tabla
+                        // contentPadding = PaddingValues(horizontal = 14.dp)
+                    ) {
+                        items(20) { index ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 16.dp, vertical = 4.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+
+                                ) {
+                                Text(
+                                    text = "Gasto ${index + 1}",
+                                    fontSize = 15.sp,
+                                    fontFamily = poppins,
+                                    fontWeight = FontWeight.Normal,
+                                    modifier = Modifier.weight(2f)
+                                )
+                                Text(
+                                    text = "S/100.00",
+                                    fontSize = 15.sp,
+                                    fontFamily = poppins,
+                                    modifier = Modifier.weight(1.5f)
+                                )
+                                Icon(
+                                    imageVector = Icons.Default.Edit,
+                                    contentDescription = "Editar",
+                                    tint = Color(0xFF000080)
+                                )
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+
     }
 }
 
